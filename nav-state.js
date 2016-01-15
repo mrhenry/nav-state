449f2e72a0360f641d89cd4a368497d8eb376f5b
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2016-01-15 15:46:57 +0100

    Fixed bind function error & export default class

--- nav-state.js
+++ nav-state.js
@@ -42,7 +42,7 @@
 /**
  * @class NavState
  */
-class NavState {
+export default class NavState {
 
 	/**
 	 * @constructor
@@ -52,7 +52,7 @@ class NavState {
 	constructor(el) {
 		this.el = el;
 
-		this.bind().check();
+		this.check();
 	}
 
 	/**
